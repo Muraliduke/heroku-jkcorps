2f8b0f54143a5b83d8e5e47167da80a7328fc50f
Muraliduke <muraliduke4461@gmail.com>
2018-06-23 20:58:05 +0530

    fix for contact page
    contact fix

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {ContactContainer} from './contact.container';
 
 import {ContactRoutingModule} from './contact-routing.module';
 
